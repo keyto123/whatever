54d14c29d052e9e977908643195792bba0c31924
Lucas <luksfreestyle_m_e_@hotmail.com>
2017-11-04 20:49:11 -0300

    Update player.hpp

--- SomeGame/player.hpp
+++ SomeGame/player.hpp
@@ -1,7 +1,8 @@
 #ifndef SOMEGAME_PLAYER_H_INCLUDED
 #define SOMEGAME_PLAYER_H_INCLUDED
 
-#include "stats.h"
+#include "stats.hpp"
+#include "alive.hpp"
 
 const int MAX_EXP = 99999;
 
@@ -9,18 +10,15 @@ enum STATS {
 	STR, VIT, AGI, DEX, LUK
 };
 
-class Player {
+class Player: public Alive {
 public:
 	Player(const char* name);
-	char* get_name();
 	int exp_up(int exp_gain);
 	int set_exp(int exp);
 	int level_up();
 	int stat_up(int stat, int rnd);
 	int get_stats(int stat);
-	Stats stats;
 private:
-	char* name;
 	int total_exp;
 	int level_exp;
 };
